49d31c55d2d8372fac597bafa17fb48278cb9efd
Yang Fei <yangfei0777@gmail.com>
2021-06-02 19:08:58 +1000

    next frontend

---
+++ frontend/pages/index.js
@@ -0,0 +1,38 @@
+import styles from '../styles/Home.module.css'
+import { gql } from "@apollo/client";
+import client from "../apollo-client";
+
+export default function Home({ countries }) {
+  return (
+    <div className={styles.grid}>
+      {countries.map((country) => (
+        <div key={country.code} className={styles.card}>
+          <h3>{country.name}</h3>
+          <p>
+            {country.code} - {country.emoji}
+          </p>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export async function getStaticProps() {
+  const { data } = await client.query({
+    query: gql`
+      query Countries {
+        countries {
+          code
+          name
+          emoji
+        }
+      }
+    `,
+  });
+
+  return {
+    props: {
+      countries: data.countries.slice(0, 4),
+    },
+ };
+}
